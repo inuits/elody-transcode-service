e0200f36dbb3f90f8cb2e6d3243760b7ccfaba0b
Gijs Vermeulen <gverm@inuits.eu>
2022-02-18 14:25:53 +0100

    Refs #86840 implement transcoding to JPEG

--- api/app.py
+++ api/app.py
@@ -50,22 +50,26 @@ rabbit.init_app(app, "basic", json.loads, json.dumps)
 
 @rabbit.queue("dams.file_uploaded")
 def start_file_transcode(routing_key, body, message_id):
-    accepted_mimetypes = ["image/jpeg", "image/tiff", "image/bmp", "image/gif"]
-    body_dict = json.loads(body)
-    if "mimetype" not in body_dict or "file_location" not in body_dict:
+    accepted_mimetypes = [
+        "image/bmp",
+        "image/gif",
+        "image/jpeg",
+        "image/png",
+        "image/tiff",
+    ]
+    data = body["data"]
+    if "mediafile" not in data or "mimetype" not in data or "url" not in data:
         return True
-    if body_dict["mimetype"] not in accepted_mimetypes:
+    if data["mimetype"] not in accepted_mimetypes:
         return True
     # job = job_helper.create_new_job("Import csv", "import csv")
     # job = job_helper.progress_job(job)
     try:
-
-        file_location = body_dict["data"]["file_location"]
-        transcoder = Transcoder(file_location)
+        transcoder = Transcoder(data["mediafile"], data["url"])
         transcoder.transcode_to_jpeg()
         # job_helper.finish_job(job)
     except Exception as ex:
-        logger.error(f"Starting import failed with: {ex}")
+        logger.error(f'Transcoding {data["mediafile"]["filename"]} failed with: {ex}')
         # job_helper.fail_job(job, str(ex))
     return True
 
--- api/transcoder.py
+++ api/transcoder.py
@@ -1,28 +1,25 @@
 import cv2
+import io
 import numpy
 import os
 import requests
 
 
 class Transcoder:
-    def __init__(self, file_location):
-        self.file_location = file_location
-        self.headers = {f'Authorization": "Bearer {os.getenv("STATIC_JWT", "None")}'}
-
-    def _get_request(self, file_location):
-        for i in range(3):
-            try:
-                req = requests.get(file_location, headers=self.headers)
-                break
-            except requests.exceptions.ConnectionError:
-                continue
-        if req.status_code != 200:
-            raise Exception(f"GET to {file_location} with headers {req.headers} failed with {req.json()}")
-        return req.json()
+    def __init__(self, mediafile, url):
+        self.headers = {"Authorization": f'Bearer {os.getenv("STATIC_JWT", "None")}'}
+        self.mediafile = mediafile
+        self.storage_api_url = os.getenv("STORAGE_API_URL", "http://storage-api:8001")
+        self.url = url
 
     def transcode_to_jpeg(self):
-        img_bytes = self._get_request(self.file_location)
-        numpy_array = numpy.frombuffer(img_bytes, numpy.uint8)
-        opencv_img = cv2.imdecode(numpy_array, cv2.IMREAD_COLOR)
-        transcoded_img_bytes = cv2.imencode(".jpg", opencv_img)
-        # make sure transcoded img is uploaded
+        req = requests.get(self.url, headers=self.headers)
+        src_np_arr = numpy.frombuffer(req.content, numpy.uint8)
+        opencv_img = cv2.imdecode(src_np_arr, cv2.IMREAD_COLOR)
+        retval, ret_np_arr = cv2.imencode(".jpg", opencv_img)
+        file = {"file": io.BytesIO(ret_np_arr.tobytes())}
+        requests.post(
+            f'{self.storage_api_url}/upload/transcode?id={self.mediafile["_key"]}',
+            files=file,
+            headers=self.headers,
+        )
