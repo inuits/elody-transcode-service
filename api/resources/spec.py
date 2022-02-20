b0489783e5a4a9934c6023dc01bd27a4e47b145c
Gijs Vermeulen <gverm@inuits.eu>
2022-02-20 14:31:19 +0100

    Fix import in spec resource

--- api/resources/spec.py
+++ api/resources/spec.py
@@ -1,12 +1,12 @@
 from flask import send_from_directory
-from resources.base_resource import BaseResource
+from flask_restful import Resource
 
 
-class OpenAPISpec(BaseResource):
+class OpenAPISpec(Resource):
     def get(self):
         return send_from_directory("docs", "dams-transcode-service.json")
 
 
-class AsyncAPISpec(BaseResource):
+class AsyncAPISpec(Resource):
     def get(self):
         return send_from_directory("docs", "dams-transcode-service-events.html")
