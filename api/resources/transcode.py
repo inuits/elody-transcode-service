f0535fe78a413a831acb05f2008143d16a370700
Gijs Vermeulen <gverm@inuits.eu>
2022-03-10 12:57:17 +0000

    Revert "Add permissions"
    This reverts commit fe6c735c32dea2890e5bd154bc9d7f6fe1d52d8f

--- api/resources/transcode.py
+++ api/resources/transcode.py
@@ -32,7 +32,7 @@ def _check_valid_content(content):
 
 
 class Transcode(Resource):
-    @app.require_oauth("transcode-mediafile")
+    @app.require_oauth()
     def post(self):
         content = _get_request_body()
         try:
