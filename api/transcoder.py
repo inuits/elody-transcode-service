5316ca5946effa4ae846868c03a84e30d3c2875a
Gijs Vermeulen <gverm@inuits.eu>
2022-08-04 16:07:44 +0200

    Remove OpenCV path
    Pillow provides better compatibility and speed is irrelevant for this
    service. This also allows us to start using Alpine for this service.

--- api/transcoder.py
+++ api/transcoder.py
@@ -1,7 +1,4 @@
-import app
-import cv2
 import io
-import numpy
 import os
 import requests
 import tempfile
@@ -22,103 +19,41 @@ class Transcoder:
         self.storage_api_url = os.getenv("STORAGE_API_URL", "http://storage-api:8001")
         self.url = url
 
-    def _get_file(self):
-        req = requests.get(self.url, headers=self.headers)
-        if req.status_code != 200:
-            raise Exception(req.json())
-        return req.content
-
-    def _patch_mediafile(self, payload):
-        req = requests.patch(
-            f"{self.collection_api_url}/mediafiles/{self._get_raw_id(self.mediafile)}",
-            json=payload,
-            headers=self.headers,
-        )
-        if req.status_code != 201:
-            raise Exception(req.json())
-
-    def _process_file(self, file):
-        src_np_arr = numpy.frombuffer(file, numpy.uint8)
-        return cv2.imdecode(src_np_arr, cv2.IMREAD_COLOR)
-
-    def _get_raw_id(self, item):
-        return item["_key"] if "_key" in item else item["_id"]
-
-    def _get_image_width_height_pil(self, image):
-        try:
-            img = Image.open(io.BytesIO(image))
-            return img.width, img.height
-        except:
-            return None, None
-
-    def _get_image_width_height_opencv(self, image):
-        try:
-            img = self._process_file(image)
-            height = img.shape[0]
-            width = img.shape[1]
-            return width, height
-        except:
-            return None, None
-
     def add_image_width_height(self):
-        image = self._get_file()
-        width, height = self._get_image_width_height_opencv(image)
-        if not width or not height:
-            app.logger.error(
-                "Failed to get width and/or height using OpenCV, trying PIL"
-            )
-            width, height = self._get_image_width_height_pil(image)
-        if not width or not height:
+        with Image.open(io.BytesIO(self.__get_file())) as img:
+            data = {"img_width": img.width, "img_height": img.width}
+        if not data["img_width"] or not data["img_height"]:
             raise Exception("Could not get width and/or height")
-        data = {"img_width": width, "img_height": height}
-        self._patch_mediafile(data)
+        self.__patch_mediafile(data)
 
     def add_video_width_height(self):
-        with tempfile.NamedTemporaryFile() as temp:
-            temp.write(self._get_file())
-            vcap = cv2.VideoCapture(temp.name)
-            width = int(vcap.get(cv2.CAP_PROP_FRAME_WIDTH))
-            height = int(vcap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-            data = {"img_width": width, "img_height": height}
-            self._patch_mediafile(data)
+        with tempfile.TemporaryDirectory() as temp_dir:
+            read_location = os.path.join(temp_dir, self.mediafile["filename"])
+            with open(read_location, "wb") as read_file:
+                read_file.write(self.__get_file())
+            c = Converter()
+            info = c.probe(read_location)
+            data = {
+                "img_width": info.video.video_width,
+                "img_height": info.video.video_height,
+            }
+        self.__patch_mediafile(data)
 
-    def _upload_transcode(self, file_name, file_bytes):
-        req = requests.post(
-            f'{self.storage_api_url}/upload/transcode?id={self.mediafile["_key"]}',
-            files={"file": (file_name, file_bytes)},
-            headers=self.headers,
-        )
-        if req.status_code != 201:
-            raise Exception(req.json())
-
-    def _transcode_to_jpeg_opencv(self, original_file):
-        opencv_img = self._process_file(original_file)
-        retval, ret_np_arr = cv2.imencode(".jpg", opencv_img)
-        return io.BytesIO(ret_np_arr.tobytes())
-
-    def _transcode_to_jpeg_pil(self, original_file):
-        img = Image.open(io.BytesIO(original_file))
-        out_img = img.convert("RGB")
+    def transcode_to_jpeg(self):
+        with Image.open(io.BytesIO(self.__get_file())) as img:
+            out_img = img.convert("RGB")
         new_bytes = io.BytesIO()
         out_img.save(new_bytes, "jpeg", quality=95)
         new_bytes.seek(0)
-        return new_bytes
-
-    def transcode_to_jpeg(self):
-        original_file = self._get_file()
-        try:
-            file_bytes = self._transcode_to_jpeg_opencv(original_file)
-        except:
-            app.logger.error("Failed to transcode to jpeg using OpenCV, trying PIL")
-            file_bytes = self._transcode_to_jpeg_pil(original_file)
         file_name = f'{os.path.splitext(self.mediafile["original_filename"])[0]}.jpg'
-        self._upload_transcode(file_name, file_bytes)
+        self.__upload_transcode(file_name, new_bytes)
+        out_img.close()
 
     def transcode_to_mp4(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             read_location = os.path.join(temp_dir, self.mediafile["filename"])
             with open(read_location, "wb") as read_file:
-                read_file.write(self._get_file())
+                read_file.write(self.__get_file())
             new_file_name = (
                 f'{os.path.splitext(self.mediafile["original_filename"])[0]}.mp4'
             )
@@ -144,4 +79,31 @@ class Transcoder:
             for _ in conv:
                 pass
             with open(write_location, "rb") as write_file:
-                self._upload_transcode(new_file_name, write_file.read())
+                self.__upload_transcode(new_file_name, write_file.read())
+
+    def __get_file(self):
+        req = requests.get(self.url, headers=self.headers)
+        if req.status_code != 200:
+            raise Exception(req.json())
+        return req.content
+
+    def __get_raw_id(self, item):
+        return item["_key"] if "_key" in item else item["_id"]
+
+    def __patch_mediafile(self, payload):
+        req = requests.patch(
+            f"{self.collection_api_url}/mediafiles/{self.__get_raw_id(self.mediafile)}",
+            json=payload,
+            headers=self.headers,
+        )
+        if req.status_code != 201:
+            raise Exception(req.json())
+
+    def __upload_transcode(self, file_name, file_bytes):
+        req = requests.post(
+            f'{self.storage_api_url}/upload/transcode?id={self.mediafile["_key"]}',
+            files={"file": (file_name, file_bytes)},
+            headers=self.headers,
+        )
+        if req.status_code != 201:
+            raise Exception(req.json())
