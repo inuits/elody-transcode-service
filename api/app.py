3ad4e8347f5a3fac631d9a4c7eb259bd727ad160
Petr Král <petr@inuits.eu>
2022-09-15 12:05:29 +0000

    Fixes #99520 add mp3 transcode for all audio types

--- api/app.py
+++ api/app.py
@@ -82,13 +82,19 @@ if os.getenv("HEALTH_CHECK_EXTERNAL_SERVICES", True) in ["True", "true", True]:
 app.add_url_rule("/health", "healthcheck", view_func=lambda: health.run())
 
 from resources.spec import AsyncAPISpec, OpenAPISpec
-from resources.transcode import JpegTranscode, MP4Transcode, WidthHeightTranscode
+from resources.transcode import (
+    JpegTranscode,
+    MP3Transcode,
+    MP4Transcode,
+    WidthHeightTranscode,
+)
 import resources.queues
 
 api.add_resource(AsyncAPISpec, "/spec/dams-transcode-service-events.html")
 api.add_resource(OpenAPISpec, "/spec/dams-transcode-service.json")
 
 api.add_resource(JpegTranscode, "/transcode/jpeg")
+api.add_resource(MP3Transcode, "/transcode/mp3")
 api.add_resource(MP4Transcode, "/transcode/mp4")
 api.add_resource(WidthHeightTranscode, "/transcode/widthheight")
 
--- api/resources/transcode.py
+++ api/resources/transcode.py
@@ -56,6 +56,24 @@ class MP4Transcode(Resource):
         )
 
 
+class MP3Transcode(Resource):
+    @app.require_oauth("transcode-to-mp3")
+    def post(self):
+        content = _get_request_body()
+        _check_valid_content(content, ["audio/"])
+        if content["madiafile"]["filename"].lower().endswith("mp3"):
+            abort(400, message="Audio file is mp3 already")
+        try:
+            transcoder = Transcoder(content["mediafile"], content["url"])
+            transcoder.transcode_to_mp3()
+        except Exception as ex:
+            return str(ex), 400
+        return (
+            f'Successfully transcoded {content["mediafile"]["filename"]} to mp3',
+            201,
+        )
+
+
 class WidthHeightTranscode(Resource):
     @app.require_oauth("transcode-add-width-height")
     def post(self):
