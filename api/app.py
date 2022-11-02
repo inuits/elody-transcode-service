a17466f502c018b551e2d6264dadbbe3602316ce
Gijs Vermeulen <gverm@inuits.eu>
2022-11-02 10:07:45 +0100

    Remove tracing code

--- api/app.py
+++ api/app.py
@@ -8,18 +8,8 @@ from flask_restful import Api
 from flask_swagger_ui import get_swaggerui_blueprint
 from healthcheck import HealthCheck
 from inuits_jwt_auth.authorization import JWTValidator, MyResourceProtector
-from inuits_otel_tracer.tracer import Tracer
 from rabbitmq_pika_flask import RabbitMQ
 from sentry_sdk.integrations.flask import FlaskIntegration
-
-traceObject = Tracer(
-    os.getenv("OTEL_ENABLED", False) in ["True", "true", True],
-    "Base Transcoder",
-    __name__,
-)
-traceObject.configTracer(
-    endpoint=os.getenv("OTLP_EXPORTER_ENDPOINT", "otel-collector:4317"), isInsecure=True
-)
 
 if os.getenv("SENTRY_ENABLED", False):
     sentry_sdk.init(
@@ -51,8 +41,6 @@ logging.basicConfig(
     level=logging.INFO,
 )
 logger = logging.getLogger(__name__)
-
-traceObject.startAutoInstrumentation(app)
 
 rabbit = RabbitMQ()
 rabbit.init_app(app, "basic", json.loads, json.dumps)
