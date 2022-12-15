3522b53680511fdf8b7174b6548b8730e7121092
Gijs Vermeulen <gverm@inuits.eu>
2022-12-15 16:13:12 +0100

    Only import Sentry libs when it's enabled

--- api/app.py
+++ api/app.py
@@ -1,7 +1,6 @@
 import json
 import logging
 import os
-import sentry_sdk
 
 from flask import Flask
 from flask_restful import Api
@@ -9,9 +8,11 @@ from flask_swagger_ui import get_swaggerui_blueprint
 from healthcheck import HealthCheck
 from inuits_jwt_auth.authorization import JWTValidator, MyResourceProtector
 from rabbitmq_pika_flask import RabbitMQ
-from sentry_sdk.integrations.flask import FlaskIntegration
 
 if os.getenv("SENTRY_ENABLED", False):
+    import sentry_sdk
+    from sentry_sdk.integrations.flask import FlaskIntegration
+
     sentry_sdk.init(
         dsn=os.getenv("SENTRY_DSN"),
         integrations=[FlaskIntegration()],
