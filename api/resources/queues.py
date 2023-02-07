db142033a715ac743854653598efd465c5780953
Gijs Vermeulen <gverm@inuits.eu>
2023-02-07 15:50:11 +0100

    Add __do_transcode helper to perform transcodes in queues

--- api/resources/queues.py
+++ api/resources/queues.py
@@ -12,60 +12,37 @@ def __is_malformed_message(data, fields, mimetypes):
     return False
 
 
-@app.rabbit.queue("dams.file_uploaded")
-def transcode_add_width_height(routing_key, body, message_id):
+def __do_transcode(body, operation, mimetypes, error_message):
     data = body["data"]
-    if __is_malformed_message(
-        data, ["mediafile", "mimetype", "url"], ["image/", "video/"]
-    ):
+    if __is_malformed_message(data, ["mediafile", "mimetype", "url"], mimetypes):
         return
     try:
         transcoder = Transcoder(data["mediafile"], data["url"])
-        transcoder.transcode("width_height")
+        transcoder.transcode(operation)
     except Exception as ex:
-        message = (
-            f'Adding dimensions for {data["mediafile"]["filename"]} failed with: {ex}'
-        )
-        app.logger.error(message)
+        app.logger.error(f'{error_message.format(data["mediafile"]["filename"])} {ex}')
+
+
+@app.rabbit.queue("dams.file_uploaded")
+def transcode_add_width_height(routing_key, body, message_id):
+    __do_transcode(
+        body,
+        "width_height",
+        ["image/", "video/"],
+        "Adding width & height for {} failed with:",
+    )
 
 
 @app.rabbit.queue("dams.file_uploaded")
 def transcode_to_jpeg(routing_key, body, message_id):
-    data = body["data"]
-    if __is_malformed_message(data, ["mediafile", "mimetype", "url"], ["image/"]):
-        return
-    try:
-        transcoder = Transcoder(data["mediafile"], data["url"])
-        transcoder.transcode("jpg")
-    except Exception as ex:
-        message = f'Transcoding {data["mediafile"]["filename"]} failed with: {ex}'
-        app.logger.error(message)
+    __do_transcode(body, "jpg", ["image/"], "Transcoding {} to jpeg failed with:")
 
 
 @app.rabbit.queue("dams.file_uploaded")
 def transcode_to_mp3(routing_key, body, message_id):
-    data = body["data"]
-    if (
-        __is_malformed_message(data, ["mediafile", "mimetype", "url"], ["audio/"])
-        or data["mimetype"] == "audio/mpeg"
-    ):
-        return
-    try:
-        transcoder = Transcoder(data["mediafile"], data["url"])
-        transcoder.transcode("mp3")
-    except Exception as ex:
-        message = f'Transcoding {data["mediafile"]["filename"]} failed with: {ex}'
-        app.logger.error(message)
+    __do_transcode(body, "mp3", ["audio/"], "Transcoding {} to mp3 failed with:")
 
 
 @app.rabbit.queue("dams.file_uploaded")
 def transcode_to_mp4(routing_key, body, message_id):
-    data = body["data"]
-    if __is_malformed_message(data, ["mediafile", "mimetype", "url"], ["video/"]):
-        return
-    try:
-        transcoder = Transcoder(data["mediafile"], data["url"])
-        transcoder.transcode("mp4")
-    except Exception as ex:
-        message = f'Transcoding {data["mediafile"]["filename"]} failed with: {ex}'
-        app.logger.error(message)
+    __do_transcode(body, "mp4", ["video/"], "Transcoding {} to mp4 failed with:")
