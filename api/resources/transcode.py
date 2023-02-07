804f17e723c044976f09e273a01f1cd7a885a0af
Gijs Vermeulen <gverm@inuits.eu>
2023-02-07 16:34:25 +0100

    Add BaseTranscode helper class to perform transcodes in http endpoints

--- api/resources/transcode.py
+++ api/resources/transcode.py
@@ -5,80 +5,54 @@ from flask_restful import abort, Resource
 from transcoder import Transcoder
 
 
-def _is_malformed_message(data, fields, mimetypes):
-    if not all(x in data for x in fields):
-        abort(405, message="Malformed request body")
-    if not any(x in data["mediafile"]["mimetype"] for x in mimetypes):
-        abort(405, message="Mimetype not allowed")
+class BaseTranscode(Resource):
+    def __get_request_body(self):
+        if request_body := request.get_json(silent=True):
+            return request_body
+        abort(405, message="Invalid input")
 
+    def __is_malformed_message(self, data, fields, mimetypes):
+        if not all(x in data for x in fields):
+            abort(405, message="Malformed request body")
+        if not any(x in data["mediafile"]["mimetype"] for x in mimetypes):
+            abort(405, message="Mimetype not allowed")
 
-def _get_request_body():
-    if request_body := request.get_json(silent=True):
-        return request_body
-    abort(405, message="Invalid input")
-
-
-class JpegTranscode(Resource):
-    @app.require_oauth("transcode-to-jpeg")
-    def post(self):
-        content = _get_request_body()
-        _is_malformed_message(content, ["image/"])
-        try:
-            transcoder = Transcoder(content["mediafile"], content["url"])
-            transcoder.transcode("jpg")
-        except Exception as ex:
-            return str(ex), 400
-        return (
-            f'Successfully transcoded {content["mediafile"]["filename"]} to jpeg',
-            201,
+    def post(self, operation, mimetypes, message):
+        content = self.__get_request_body()
+        self.__is_malformed_message(
+            content, ["mediafile", "mimetype", "url"], mimetypes
         )
-
-
-class MP3Transcode(Resource):
-    @app.require_oauth("transcode-to-mp3")
-    def post(self):
-        content = _get_request_body()
-        _is_malformed_message(content, ["audio/"])
-        if content["mimetype"] == "audio/mpeg":
+        if operation == "mp3" and content["mimetype"] == "audio/mpeg":
             abort(400, message=f'{content["mediafile"]["filename"]} is already an mp3')
         try:
             transcoder = Transcoder(content["mediafile"], content["url"])
-            transcoder.transcode("mp3")
+            transcoder.transcode(operation)
         except Exception as ex:
             return str(ex), 400
-        return (
-            f'Successfully transcoded {content["mediafile"]["filename"]} to mp3',
-            201,
-        )
+        return message.format(content["mediafile"]["filename"]), 201
 
 
-class MP4Transcode(Resource):
+class JpegTranscode(BaseTranscode):
+    @app.require_oauth("transcode-to-jpeg")
+    def post(self):
+        return super().post("jpg", ["image/"], "Successfully transcoded {} to jpeg")
+
+
+class MP3Transcode(BaseTranscode):
+    @app.require_oauth("transcode-to-mp3")
+    def post(self):
+        return super().post("mp3", ["audio/"], "Successfully transcoded {} to mp3")
+
+
+class MP4Transcode(BaseTranscode):
     @app.require_oauth("transcode-to-mp4")
     def post(self):
-        content = _get_request_body()
-        _is_malformed_message(content, ["video/"])
-        try:
-            transcoder = Transcoder(content["mediafile"], content["url"])
-            transcoder.transcode("mp4")
-        except Exception as ex:
-            return str(ex), 400
-        return (
-            f'Successfully transcoded {content["mediafile"]["filename"]} to mp4',
-            201,
-        )
+        return super().post("mp4", ["video/"], "Successfully transcoded {} to mp4")
 
 
-class WidthHeightTranscode(Resource):
+class WidthHeightTranscode(BaseTranscode):
     @app.require_oauth("transcode-add-width-height")
     def post(self):
-        content = _get_request_body()
-        _is_malformed_message(content, ["image/", "video/"])
-        try:
-            transcoder = Transcoder(content["mediafile"], content["url"])
-            transcoder.transcode("width_height")
-        except Exception as ex:
-            return str(ex), 400
-        return (
-            f'Successfully added {content["mediafile"]["filename"]} width & height',
-            201,
+        return super().post(
+            "width_height", ["image/", "video/"], "Successfully added {} width & height"
         )
