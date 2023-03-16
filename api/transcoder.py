0053f5a11a4850afe63d475a108cd23c4ef7f083
Gijs Vermeulen <gverm@inuits.eu>
2023-03-16 11:17:08 +0100

    Make Transcoder a Singleton

--- api/transcoder.py
+++ api/transcoder.py
@@ -11,7 +11,16 @@ from PIL import ExifTags, Image, ImageOps, TiffImagePlugin
 Image.MAX_IMAGE_PIXELS = None
 
 
-class Transcoder:
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Transcoder(metaclass=Singleton):
     def __init__(self):
         self.collection_api_url = os.getenv("COLLECTION_API_URL")
         self.headers = {"Authorization": f'Bearer {os.getenv("STATIC_JWT")}'}
