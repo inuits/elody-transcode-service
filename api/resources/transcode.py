187ed82d61f2b35d75f3fc895ca180caadea7b02
Bram Oosterlynck <booster@inuits.eu>
2023-10-18 15:00:14 +0200

    Protect some functions

--- api/resources/transcode.py
+++ api/resources/transcode.py
@@ -7,26 +7,26 @@ from transcoder import Transcoder
 
 
 class BaseTranscode(Resource):
-    def __get_auth_headers(self):
-        tenant = request.headers.get("apikey")
-        if tenant:
-            return {"apikey": tenant}
-        else:
-            return {"Authorization": f'Bearer {os.getenv("STATIC_JWT")}'}
-
-    def __get_request_body(self):
-        if request_body := request.get_json(silent=True):
-            return request_body
-        abort(405, message="Invalid input")
-
     def __is_malformed_message(self, data, fields, mimetypes):
         if not all(x in data for x in fields):
             abort(405, message="Malformed request body")
         if not any(x in data["mediafile"]["mimetype"] for x in mimetypes):
             abort(405, message="Mimetype not allowed")
 
+    def _get_auth_headers(self):
+        tenant = request.headers.get("apikey")
+        if tenant:
+            return {"apikey": tenant}
+        else:
+            return {"Authorization": f'Bearer {os.getenv("STATIC_JWT")}'}
+
+    def _get_request_body(self):
+        if request_body := request.get_json(silent=True):
+            return request_body
+        abort(405, message="Invalid input")
+
     def post(self, operation, mimetypes, message):
-        content = self.__get_request_body()
+        content = self._get_request_body()
         self.__is_malformed_message(content, ["mediafile", "mimetype"], mimetypes)
         if operation == "mp3" and content["mimetype"] == "audio/mpeg":
             abort(400, message=f'{content["mediafile"]["filename"]} is already an mp3')
@@ -34,7 +34,7 @@ class BaseTranscode(Resource):
             Transcoder().transcode(
                 content["mediafile"],
                 operation,
-                self.__get_auth_headers(),
+                self._get_auth_headers(),
             )
         except Exception as ex:
             return str(ex), 400
