5506d8e8fdec02e525ecc8e03276511a688097de
Tim Standaert <tim.standaert@inuits.eu>
2026-04-03 09:24:55 +0200

    chore: format entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,28 +3,28 @@
 set -e
 
 if [ ! -z "$@" ]; then
-  echo "Running command: $@"
-  exec $@
-  exit $?
+	echo "Running command: $@"
+	exec $@
+	exit $?
 fi
 
 if [ ! -z "$TRUSTED_CA_BUNDLE" ]; then
-  cp /etc/ssl/certs/ca-certificates.crt /tmp/ca-certificates.crt
-  echo "${TRUSTED_CA_BUNDLE}" >> /tmp/ca-certificates.crt
-  export CURL_CA_BUNDLE="/tmp/ca-certificates.crt"
+	cp /etc/ssl/certs/ca-certificates.crt /tmp/ca-certificates.crt
+	echo "${TRUSTED_CA_BUNDLE}" >>/tmp/ca-certificates.crt
+	export CURL_CA_BUNDLE="/tmp/ca-certificates.crt"
 fi
 
 if [ "$APP_ENV" = "dev" ]; then
-  echo "Clearing /tmp"
+	echo "Clearing /tmp"
 	rm -rf /tmp/* 2>/dev/null || true
-  echo "Starting development server..."
-  export FLASK_DEBUG='1'
-  cd ~/api
-  exec ~/.local/bin/flask run --host=0.0.0.0
+	echo "Starting development server..."
+	export FLASK_DEBUG='1'
+	cd ~/api
+	exec ~/.local/bin/flask run --host=0.0.0.0
 else
-  echo "Clearing /tmp"
+	echo "Clearing /tmp"
 	rm -rf /tmp/* 2>/dev/null || true
-  echo "Starting gunicorn server..."
-  cd ~/api
-  exec ~/.local/bin/gunicorn -b 0.0.0.0 --keep-alive 30 --access-logfile - --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(M)sms %(b)s "%(f)s" "%(a)s"' "app:app"
+	echo "Starting gunicorn server..."
+	cd ~/api
+	exec ~/.local/bin/gunicorn -b 0.0.0.0 --keep-alive 30 --access-logfile - --access-logformat '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(M)sms %(b)s "%(f)s" "%(a)s"' "app:app"
 fi
