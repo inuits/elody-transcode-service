c236a8fef5688db49a0eab2ba97f3db0eee56af3
Tim Standaert <tim.standaert@inuits.eu>
2026-07-30 11:03:38 +0200

    feat(typing): add some initial types for return signatures

---
+++ api/elody_types/__init__.py
@@ -0,0 +1,3 @@
+from .types import MediafileEntity
+
+__all__ = ["MediafileEntity"]
---
+++ api/elody_types/types.py
@@ -0,0 +1,23 @@
+from typing import Any, TypedDict
+
+
+class MetadataObject(TypedDict):
+    key: str
+    value: Any
+
+
+class RelationObject(TypedDict):
+    key: str
+    type: str
+
+
+class ElodyEntity(TypedDict):
+    _id: str
+    id: str
+    metadata: list[MetadataObject]
+    relations: list[RelationObject]
+    type: str
+
+
+class MediafileEntity(ElodyEntity):
+    original_file_location: str
