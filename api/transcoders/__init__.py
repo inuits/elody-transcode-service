703aba27a2e918a8c4c3b3a63524b799f8594946
Tim Standaert <tim.standaert@inuits.eu>
2026-08-13 11:44:31 +0200

    fix: remove accidental import

--- api/transcoders/__init__.py
+++ api/transcoders/__init__.py
@@ -1,7 +1,6 @@
 from .jpeg_transcode_service import JPEGTranscoder as JPEGTranscoder
 from .mp3_transcode_service import MP3Transcoder as MP3Transcoder
 from .mp4_transcode_service import MP4Transcoder as MP4Transcoder
-from .pdf_transcode_service import PDFTranscoder as PDFTranscoder
 from .transcoder import Transcoder
 
 __all__ = ["Transcoder"]
